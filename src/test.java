eb9781eee5b35c3cdc2326998ad6581af1ee8d49
dlw <2642889118@qq.com>
2023-06-09 16:38:10 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,10 @@
+/**
+ * @Description:
+ * @author: Hobbit
+ * @date: 2023.06.09
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
